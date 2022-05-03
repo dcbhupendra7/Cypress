c992de78d7c16360e412c131d6bf8843ae36042b
Bhupendra <bhupendra.dangi@sevadev.com>
2022-05-03 22:20:24 +0545

    First commit

---
+++ cypress/integration/automation-store-test/contact-us.js
@@ -0,0 +1,23 @@
+describe("This is the test for contact us page of automation-store", () => {
+  it("Should display and submit the details of contact us page", () => {
+    cy.visit("https://www.automationteststore.com/");
+    // cy.get(".info_links_footer > :nth-child(5) > a").click();
+    cy.xpath("//a[contains(@href, 'contact')]").click();
+    // cy.get("#ContactUsFrm_first_name").type("Bhupendra");
+    cy.xpath("//input[contains(@id, 'ContactUsFrm_first_name')]").type(
+      "Bhupendra"
+    );
+    // cy.get("#ContactUsFrm_email").type("bhupendradangi5@gmail.com");
+    cy.xpath("//input[contains(@id, 'ContactUsFrm_email')]").type(
+      "bhupendradangi5@gmail.com"
+    );
+    // cy.get("#ContactUsFrm_enquiry").type(
+    //   "Sorry, i don't have any enquiry at the moment."
+    // );
+    cy.xpath("//textarea[@id = 'ContactUsFrm_enquiry']").type(
+      "Sorry, i don't have any enquiry at the moment."
+    );
+    // cy.get(".col-md-6 > .btn").click();
+    cy.xpath("//button[@title='Submit']").click();
+  });
+});
---
+++ cypress/integration/webdrive-uni/contact.js
@@ -0,0 +1,5 @@
+describe("Test case for web driver contact us page", () => {
+  it("Should sumbit contact us details for webdriver page", () => {
+    cy.visit("http://www.webdriveruniversity.com/");
+  });
+});
