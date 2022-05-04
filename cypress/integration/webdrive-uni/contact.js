5af3731072ab285f7024b2b16583fc552208c770
Bhupendra <bhupendra.dangi@sevadev.com>
2022-05-04 21:26:05 +0545

    SecondDay

--- cypress/integration/webdrive-uni/contact.js
+++ cypress/integration/webdrive-uni/contact.js
@@ -1,5 +1,14 @@
 describe("Test case for web driver contact us page", () => {
   it("Should sumbit contact us details for webdriver page", () => {
-    cy.visit("http://www.webdriveruniversity.com/");
+    cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    cy.get("input[name='first_name']").type("Bhupendra");
+    cy.get("input[name='last_name']").type("Dangi");
+    cy.get("input[name='email'][placeholder='Email Address']").type(
+      "bhupendradangi@gmail.com"
+    );
+    cy.get("textarea[name='message'][placeholder='Comments']").type(
+      "Hi, how are you?"
+    );
+    cy.get("input[value='SUBMIT']").click();
   });
 });
