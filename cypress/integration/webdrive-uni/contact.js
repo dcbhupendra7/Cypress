ec8096f71407f6895dc6bb5fe94e2ed3642eb889
Bhupendra <bhupendra.dangi@sevadev.com>
2022-06-10 21:58:52 +0545

    Proper use of assertion

--- cypress/integration/webdrive-uni/contact.js
+++ cypress/integration/webdrive-uni/contact.js
@@ -1,5 +1,16 @@
 describe("Test case for web driver contact us page", () => {
   it("Should sumbit contact us details for webdriver page", () => {
-    cy.visit("http://www.webdriveruniversity.com/");
+    // cy.visit("http://www.webdriveruniversity.com/");
+    // cy.get("#contact-us").should("exist").click();
+    cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html");
+    cy.get('[name="first_name"]').should("exist").type("Bhupendra");
+    cy.get('[name="last_name"]').should("exist").type("Dangi");
+    cy.get('[name="email"]').should("exist").type("bhupendradangi@gmail.com");
+    cy.get('[name="message"]')
+      .should("exist")
+      .type("Hello, How are you doing?");
+    cy.get('[type="submit"]').should("exist").click();
+    cy.contains("Thank You for your Message!");
+    cy.log("Successfully submitted.");
   });
 });
