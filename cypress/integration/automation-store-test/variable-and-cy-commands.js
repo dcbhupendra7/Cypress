8efd5f7c1bbee2c38702d384b1f8e20045c6e482
Bhupendra <bhupendra.dangi@sevadev.com>
2022-06-21 20:06:26 +0545

    Use of variables along with jquery

--- cypress/integration/automation-store-test/variable-and-cy-commands.js
+++ cypress/integration/automation-store-test/variable-and-cy-commands.js
@@ -24,13 +24,38 @@ describe("Verifying variables, cypress commands and jquery commands", () => {
     cy.get("a[href*='product/category&path=']").contains("Makeup").click();
     cy.get("a[href*='product/category&path=']").contains("Skincare").click();
   });
-  it.only("Checking Header text for makeup page", () => {
+  it("Checking Header text for makeup page", () => {
     cy.visit("https://automationteststore.com/");
     cy.get("a[href*='product/category&path=']").contains("Makeup").click();
     cy.get("h1 .maintext").then(($headerText) => {
       const headerText = $headerText.text();
       console.log("Value of header text is: " + headerText);
       expect(headerText).is.eq("Makeup");
+    });
+  });
+  it.only("Validate properties of Contact Us Page", () => {
+    cy.visit("https://automationteststore.com/index.php?rt=content/contact");
+
+    //Uses cypress commands and chaining
+    cy.contains("#ContactUsFrm", "Contact Us Form")
+      .find("#field_11")
+      .should("contain", "First name");
+    cy.contains("#ContactUsFrm", "Contact Us Form")
+      .find("#field_12")
+      .should("contain", "Email");
+    cy.contains("#ContactUsFrm", "Contact Us Form")
+      .find("#field_13")
+      .should("contain", "Enquiry");
+    //JQery Approach
+    cy.contains("#ContactUsFrm", "Contact Us Form").then((text) => {
+      const firstNextText = text.find("#field_11").text();
+      expect(firstNextText).to.contain("First name");
+
+      //Embedded commands (Closure)
+      cy.get("#field_11").then((fnText) => {
+        cy.log(fnText.text());
+        cy.log(fnText);
+      });
     });
   });
 });
