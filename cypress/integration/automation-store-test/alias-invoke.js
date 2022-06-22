d6d85ce14a1e7cb155c78737921c6556fde85c2c
Bhupendra <bhupendra.dangi@sevadev.com>
2022-06-22 21:44:12 +0545

    Iterating through alias and variables

--- cypress/integration/automation-store-test/alias-invoke.js
+++ cypress/integration/automation-store-test/alias-invoke.js
@@ -20,7 +20,7 @@ describe("Use of Alias and Invoke", () => {
     cy.get("@nameOfSkinProduct").its("length").should("be.gt", 6);
     cy.get("@nameOfSkinProduct").should("include", "Flash Bronzer Body Gel");
   });
-  it.only("Impolement alias and invoke Home page", () => {
+  it("Implement alias and invoke Home page", () => {
     cy.visit("https://automationteststore.com/");
     cy.get(".thumbnail").as("lenghtOfHomePageProduct");
     cy.get("@lenghtOfHomePageProduct").should("have.length", 16);
@@ -28,5 +28,48 @@ describe("Use of Alias and Invoke", () => {
       .find(".productcart")
       .invoke("attr", "title")
       .should("include", "Add to Cart");
+  });
+  it.only("Implement alias and invoke to itirate through sell price", () => {
+    cy.visit("https://automationteststore.com/");
+    cy.get(".thumbnail").find(".oneprice").invoke("text").as("itemPrice");
+    cy.get(".thumbnail").find(".pricenew").invoke("text").as("saleItemPrice");
+    var totalSum = 0;
+    cy.get("@itemPrice").then(($el) => {
+      var productPrice = $el.split("$");
+      var i;
+      var nonSaleItemSum = 0;
+      for (i = 0; i < productPrice.length; i++) {
+        cy.log(productPrice[i]);
+        nonSaleItemSum += Number(productPrice[i]);
+      }
+      totalSum += nonSaleItemSum;
+      cy.log("Sum of non-sale product: " + nonSaleItemSum);
+    });
+    cy.get("@saleItemPrice")
+      .then(($el) => {
+        var saleProductPrice = $el.split("$");
+        var i;
+        var saleProductSum = 0;
+        for (i = 0; i < saleProductPrice.length; i++) {
+          cy.log(saleProductPrice[i]);
+          saleProductSum += Number(saleProductPrice[i]);
+        }
+        cy.log("Sum of sale product: " + saleProductSum);
+        totalSum += saleProductSum;
+      })
+      .then(() => {
+        cy.log("Sum of sale product and non-sale product: " + totalSum);
+        expect(totalSum).eq(648.5);
+      });
+  });
+  it("Implement alias and invoke to itirate through sell price", () => {
+    cy.visit("https://automationteststore.com/");
+    cy.get(".thumbnail").as("lenghtOfHomePageProduct");
+    cy.get("@lenghtOfHomePageProduct");
+    cy.get("@lenghtOfHomePageProduct")
+      .find(".pricenew")
+      .each(($el, index, $list) => {
+        cy.log($el.text());
+      });
   });
 });
