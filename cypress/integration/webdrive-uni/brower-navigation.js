db5acf0194769ff1d7732540a6192717a18fb881
Bhupendra <bhupendra.dangi@sevadev.com>
2022-06-23 20:11:31 +0545

    Switcing between tabs

---
+++ cypress/integration/webdrive-uni/brower-navigation.js
@@ -0,0 +1,24 @@
+describe("Validate webdriver homepage links", () => {
+  it("Confirm links redirect to the correct pages", () => {
+    cy.visit("http://www.webdriveruniversity.com");
+    cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
+    cy.url().should("include", "contactus");
+    cy.go("back");
+    cy.url().should("include", "http://www.webdriveruniversity.com/");
+    cy.go("forward");
+    cy.url().should("include", "contactus");
+    cy.reload();
+    cy.reload(true); //Reload without cache
+    cy.go("back");
+    cy.get("#login-portal")
+      .invoke("removeAttr", "target")
+      .click({ force: "true" });
+    cy.url().should("include", "Login-Portal");
+    cy.go("back");
+    cy.get("#to-do-list")
+      .invoke("removeAttr", "target")
+      .click({ force: "true" });
+    cy.url().should("include", "To-Do-List");
+    cy.go("back");
+  });
+});
