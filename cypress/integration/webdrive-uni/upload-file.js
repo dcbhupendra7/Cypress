8c54d37338b498021e326f971d9d0c3ef80cd3fe
Bhupendra <bhupendra.dangi@sevadev.com>
2022-07-13 21:50:06 +0545

    File upload

---
+++ cypress/integration/webdrive-uni/upload-file.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+describe("Test File Upload via webdriveruni", () => {
+  it("Upload a file via web drive uni", () => {
+    cy.visit("http://www.webdriveruniversity.com");
+    cy.get("#file-upload")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+
+    cy.fixture("laptop.png", "base64").then((fileContent) => {
+      cy.get("#myFile").attachFile(
+        {
+          fileContent,
+          fileName: "laptop.png",
+          mimeType: "image/png",
+        },
+        {
+          uploadType: "input",
+        }
+      );
+    });
+    cy.get("#submit-button").click();
+  });
+  it("Upload a no file via web drive uni", () => {
+    cy.visit("http://www.webdriveruniversity.com");
+    cy.get("#file-upload")
+      .invoke("removeAttr", "target")
+      .click({ force: true });
+
+    cy.get("#submit-button").click();
+  });
+});
