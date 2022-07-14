f9b504ff403d08b99472f22b7328d3828a5acd7d
Bhupendra <bhupendra.dangi@sevadev.com>
2022-07-14 19:58:21 +0545

    use of hooks

--- cypress/integration/webdrive-uni/radio-buttons.js
+++ cypress/integration/webdrive-uni/radio-buttons.js
@@ -1,17 +1,15 @@
 describe("Verify radio buttons via webuni", () => {
-  it("Check specific radio button", () => {
+  before(() => {
     cy.visit("http://www.webdriveruniversity.com");
     cy.get("#dropdown-checkboxes-radiobuttons")
       .invoke("removeAttr", "target")
       .click({ force: true });
+  });
+  it("Check specific radio button", () => {
     cy.get("#radio-buttons").find("[type='radio']").as("button-value");
     cy.get("@button-value").eq(1).check().should("be.checked");
   });
-  it.only("Validate state of radio buttons", () => {
-    cy.visit("http://www.webdriveruniversity.com");
-    cy.get("#dropdown-checkboxes-radiobuttons")
-      .invoke("removeAttr", "target")
-      .click({ force: true });
+  it("Validate state of radio buttons", () => {
     cy.get("input[value='lettuce']").should("be.not.checked");
     cy.get("input[value='cabbage']").should("be.disabled");
     cy.get("input[value='pumpkin']").should("be.checked");
