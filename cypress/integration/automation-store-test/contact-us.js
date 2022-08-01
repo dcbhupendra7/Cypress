ae8a1127a6e17da8e521df48dbb9095c906b706d
Bhupendra <bhupendra.dangi@sevadev.com>
2022-08-01 19:23:20 +0545

    using data to fill email and name from json file

--- cypress/integration/automation-store-test/contact-us.js
+++ cypress/integration/automation-store-test/contact-us.js
@@ -1,4 +1,7 @@
 describe("This is the test for contact us page of automation-store", () => {
+  beforeEach(() => {
+    cy.fixture("userDetails").as("user");
+  });
   it("Should display and submit the details of contact us page", () => {
     cy.visit("https://www.automationteststore.com/");
     cy.get("a[href$='contact']")
@@ -7,13 +10,16 @@ describe("This is the test for contact us page of automation-store", () => {
         console.log("On clicking contact us: " + className.text());
       });
     // cy.xpath("//a[contains(@href, 'contact')]").click();
-    cy.get("#ContactUsFrm_first_name").type("Bhupendra");
+    cy.get("@user").then((user) => {
+      cy.get("#ContactUsFrm_first_name").type(user.first_name);
+      cy.get("#ContactUsFrm_email")
+        .should("have.attr", "name", "email")
+        .type(user.email);
+    });
+
     // cy.xpath("//input[contains(@id, 'ContactUsFrm_first_name')]").type(
     //   "Bhupendra"
 
-    cy.get("#ContactUsFrm_email")
-      .should("have.attr", "name", "email")
-      .type("bhupendradangi5@gmail.com");
     // cy.xpath("//input[contains(@id, 'ContactUsFrm_email')]").type(
     //   "bhupendradangi5@gmail.com"
     // );
