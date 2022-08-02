1b5020a61141c3511e720e478adbbfc48320e7a0
Bhupendra <bhupendra.dangi@sevadev.com>
2022-08-02 21:15:01 +0545

    adding base url into cypress config file using environment variables

--- cypress/e2e/webdrive-uni/contact.cy.js
+++ cypress/e2e/webdrive-uni/contact.cy.js
@@ -1,10 +1,11 @@
+/// <reference types="cypress" />
+
 describe("Test case for web driver contact us page", () => {
   beforeEach(() => {
-    cy.visit("http://www.webdriveruniversity.com");
-    cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
-    cy.document().should("have.property", "charset").and("eq", "UTF-8");
-    cy.title().should("include", "WebDriver | Contact Us");
-    cy.url().should("include", "contactus");
+    cy.visit(Cypress.env("web_driverUniHome" + "/Contact-Us/contactus.html"));
+    // cy.document().should("have.property", "charset").and("eq", "UTF-8");
+    // cy.title().should("include", "WebDriver | Contact Us");
+    // cy.url().should("include", "contactus");
   });
   before(() => {
     cy.fixture("example").then((data) => {
@@ -14,20 +15,20 @@ describe("Test case for web driver contact us page", () => {
   });
   it("Should sumbit contact us details for webdriver page", () => {
     cy.webDriverUni_ContactForm_Submission(
-      data.first_name,
-      data.last_name,
-      data.email,
-      data.body,
+      Cypress.env("first_name"),
+      Cypress.env("last_name"),
+      Cypress.env("email"),
+      Cypress.env("message"),
       "h1",
       "Thank You for your Message!"
     );
   });
   it("Should not be able to sumbit contact us details for webdriver page", () => {
     cy.webDriverUni_ContactForm_Submission(
-      data.first_name,
-      data.last_name,
+      Cypress.env("first_name"),
+      Cypress.env("last_name"),
       " ",
-      data.body,
+      Cypress.env("message"),
       "body",
       "Error: Invalid email address"
     );
