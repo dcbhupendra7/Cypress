6cf620bfeac0376f04a53fa942305f6f5efdb3ed
Bhupendra <bhupendra.dangi@sevadev.com>
2022-08-04 21:40:32 +0545

    report generation with mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "zu2dey",
   chromeWebSecurity: false,
   defaultCommandTimeout: 10000,
   pageLoadTimeout: 60000,
 
   e2e: {
     baseUrl: "http://www.webdriveruniversity.com",
-    viewportHeight: 500,
-    viewportWidth: 400,
+    // viewportHeight: 500,
+    // viewportWidth: 400,
 
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
@@ -21,5 +22,9 @@ module.exports = defineConfig({
     last_name: "Curry",
     email: "johncurry@gmail.com",
     message: "Hi, i am john curry!!!!!!! How are you?",
+  },
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    configFile: "reporter-config.json",
   },
 });
